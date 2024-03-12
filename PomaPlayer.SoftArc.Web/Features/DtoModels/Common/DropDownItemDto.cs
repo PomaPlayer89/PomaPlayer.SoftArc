d54d316b45e9b878f84c919bb71667dde04233e9
aggink <aggink@mail.ru>
2024-03-12 15:44:29 +0300

    #1 Удален дублированный класс

--- PomaPlayer.SoftArc.Web/Features/DtoModels/Common/DropDownItemDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PomaPlayer.SoftArc.Web.Features.DtoModels.Common
-{
-    public sealed record DropDownItemDto
-    {
-        public string Label { get; set; }
-
-        public string Value { get; set; }
-    }
-}
